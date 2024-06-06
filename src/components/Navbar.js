24af3291e8999351cfa34d15417f6ad6784b865c
Naresh Verma <learnnaresh7566@gmail.com>
2024-06-06 18:31:59 +0530

    Add correct way to close the menu bar when click on logout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -82,7 +82,10 @@ const Navbar = () => {
                 </li>
                 <li>
                   <button
-                    onClick={handleLogout || toggleDropdown}
+                     onClick={() => {
+                      handleLogout();
+                      toggleDropdown();
+                    }}
                     className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                     role="menuitem"
                   >
